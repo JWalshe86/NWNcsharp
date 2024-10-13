f74805df272527dbca70bc4eebb4a7f33b439f0f
John Walshe <jwalshedev@gmail.com>
2024-10-13 23:10:29 +0100

    Update .gitignore to exclude .vs directory

---
+++ NagsWithNotions/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using NagsWithNotions.Models;
+using System.Diagnostics;
+
+namespace NagsWithNotions.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string name = "John")
+        {
+            ViewData["Message"] = $"Welcome, {name}!"; // Sets a welcome message
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NagsWithNotions/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NagsWithNotions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
