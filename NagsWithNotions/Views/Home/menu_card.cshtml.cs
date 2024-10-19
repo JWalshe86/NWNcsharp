982d8031a7ad7e281388194dc53560745d0601f9
John Walshe <jwalshedev@gmail.com>
2024-10-19 23:32:02 +0100

    Add about section

---
+++ NagsWithNotions/Views/Home/menu_card.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NagsWithNotions.Views.Home
+{
+    public class menu_cardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
