840f51d82dbf7009476af822db1e783fc1bb4863
John Walshe <jwalshedev@gmail.com>
2024-10-21 19:46:02 +0100

    Bug .vs

--- NagsWithNotions/obj/Debug/net8.0/NagsWithNotions.AssemblyInfo.cs
+++ NagsWithNotions/obj/Debug/net8.0/NagsWithNotions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NagsWithNotions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b59c5d2e2308d2f8a45ccbe1575cbd218addf5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf047fb12d609f778d84ddd7e027dbdbb0edff0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("NagsWithNotions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NagsWithNotions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
